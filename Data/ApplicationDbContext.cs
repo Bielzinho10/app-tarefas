d1144e68e614c07437498cfd1efef5add2699e07
Gabriel ferreira <gabriel.cruzeiro14@gmail.com>
2025-08-14 16:40:22 -0300

    [feat] Implementa delos de domínio e sistema de autenticação

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace app_tarefas.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        
+        // Exemplo: seu DbSet aqui
+        // public DbSet<Tipo> Tipos { get; set; }
+    }
+}
